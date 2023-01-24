4d834d7bc9da6013d7460c864737dc04eb5ac997
Fatbrother <kevin92422@gmail.com>
2023-01-24 22:28:37 +0800

    complete buttom

--- lib/screens/bottom.dart
+++ lib/screens/bottom.dart
@@ -1,4 +1,7 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as js;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_personal_website/utilities/design.dart';
 
 class ButtomBar extends StatelessWidget {
@@ -27,26 +30,54 @@ class ButtomBar extends StatelessWidget {
             Row(
               children: [
                 Text(
-                  'connect with me',
+                  'contact with me',
                   style: Theme.of(context).textTheme.bodySmall,
                 ),
                 const SizedBox(width: 10),
-                Image.asset(
-                  "icons/Discord.png",
-                  height: 40,
-                  width: 40,
+                IconButton(
+                  onPressed: () {
+                    String userId = "劉又瑄#8450";
+                    Clipboard.setData(ClipboardData(text: userId));
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            contentPadding: const EdgeInsets.all(10),
+                            content: const Text("User ID copied to clipboard"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("OK"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  iconSize: 40,
+                  padding: const EdgeInsets.all(0),
+                  icon: Image.asset("assets/icons/Discord.png"),
                 ),
                 const SizedBox(width: 10),
-                Image.asset(
-                  "icons/Facebook.png",
-                  height: 40,
-                  width: 40,
+                IconButton(
+                  onPressed: () {
+                    String userUrl = "https://www.instagram.com/maybeiamafrog/";
+                    js.context.callMethod("open", [userUrl]);
+                  },
+                  iconSize: 40,
+                  padding: const EdgeInsets.all(0),
+                  icon: Image.asset("assets/icons/Instagram.png"),
                 ),
                 const SizedBox(width: 10),
-                Image.asset(
-                  "icons/Instagram.png",
-                  height: 40,
-                  width: 40,
+                IconButton(
+                  onPressed: () {
+                    String userUrl = "https://www.facebook.com/profile.php?id=100007226413243";
+                    js.context.callMethod("open", [userUrl]);
+                  },
+                  iconSize: 40,
+                  padding: const EdgeInsets.all(0),
+                  icon: Image.asset("assets/icons/Facebook.png"),
                 ),
               ],
             )
