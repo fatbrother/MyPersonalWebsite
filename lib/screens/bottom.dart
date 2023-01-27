141de15c7ae9f321769f2f788066062d6bc5ef71
Fatbrother <kevin92422@gmail.com>
2023-01-28 01:07:23 +0800

    replace diolog with snackBar

--- lib/screens/bottom.dart
+++ lib/screens/bottom.dart
@@ -1,5 +1,6 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:js' as js;
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_personal_website/utilities/design.dart';
@@ -36,24 +37,34 @@ class ButtomBar extends StatelessWidget {
                 const SizedBox(width: 10),
                 IconButton(
                   onPressed: () {
+                    SnackBar snackBar = SnackBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                      padding: EdgeInsets.fromLTRB(
+                        Design.getMatirialWidth(context) * 0.4,
+                        10,
+                        Design.getMatirialWidth(context) * 0.4,
+                        10,
+                      ),
+                      content: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(70, 0, 0, 0),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            'User ID copied to clipboard',
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ),
+                      ),
+                      duration: const Duration(seconds: 1),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
                     String userId = "劉又瑄#8450";
                     Clipboard.setData(ClipboardData(text: userId));
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            contentPadding: const EdgeInsets.all(10),
-                            content: const Text("User ID copied to clipboard"),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text("OK"),
-                              )
-                            ],
-                          );
-                        });
                   },
                   iconSize: 40,
                   padding: const EdgeInsets.all(0),
@@ -72,7 +83,8 @@ class ButtomBar extends StatelessWidget {
                 const SizedBox(width: 10),
                 IconButton(
                   onPressed: () {
-                    String userUrl = "https://www.facebook.com/profile.php?id=100007226413243";
+                    String userUrl =
+                        "https://www.facebook.com/profile.php?id=100007226413243";
                     js.context.callMethod("open", [userUrl]);
                   },
                   iconSize: 40,
