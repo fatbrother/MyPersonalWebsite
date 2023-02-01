627900e9714dfd1eb5a4c8ea6b9ce32bb828fae1
Fatbrother <kevin92422@gmail.com>
2023-02-01 23:49:26 +0800

    add a icon pkg

--- lib/screens/bottom.dart
+++ lib/screens/bottom.dart
@@ -3,6 +3,7 @@ import 'dart:js' as js;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_personal_website/utilities/design.dart';
 
 class ButtomBar extends StatelessWidget {
@@ -36,39 +37,23 @@ class ButtomBar extends StatelessWidget {
                 ),
                 const SizedBox(width: 10),
                 IconButton(
-                  onPressed: () {
-                    SnackBar snackBar = SnackBar(
-                      backgroundColor: Colors.transparent,
-                      elevation: 0,
-                      padding: EdgeInsets.fromLTRB(
-                        Design.getMatirialWidth(context) * 0.4,
-                        10,
-                        Design.getMatirialWidth(context) * 0.4,
-                        10,
-                      ),
-                      content: Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(70, 0, 0, 0),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        height: 40,
-                        child: Center(
-                          child: Text(
-                            'User ID copied to clipboard',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ),
-                      ),
-                      duration: const Duration(seconds: 1),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-                    String userId = "劉又瑄#8450";
-                    Clipboard.setData(ClipboardData(text: userId));
-                  },
-                  iconSize: 40,
+                  onPressed: () => copyText("kevin92422@gmail.com", context),
+                  iconSize: 30,
                   padding: const EdgeInsets.all(0),
-                  icon: Image.asset("assets/icons/Discord.png"),
+                  icon: const Icon(
+                    FontAwesomeIcons.envelope,
+                    size: 30,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                IconButton(
+                  onPressed: () => copyText("劉又瑄#8450", context),
+                  iconSize: 30,
+                  padding: const EdgeInsets.all(0),
+                  icon: const Icon(
+                    FontAwesomeIcons.discord,
+                    size: 30,
+                  ),
                 ),
                 const SizedBox(width: 10),
                 IconButton(
@@ -76,9 +61,12 @@ class ButtomBar extends StatelessWidget {
                     String userUrl = "https://www.instagram.com/maybeiamafrog/";
                     js.context.callMethod("open", [userUrl]);
                   },
-                  iconSize: 40,
+                  iconSize: 30,
                   padding: const EdgeInsets.all(0),
-                  icon: Image.asset("assets/icons/Instagram.png"),
+                  icon: const Icon(
+                    FontAwesomeIcons.instagram,
+                    size: 30,
+                  ),
                 ),
                 const SizedBox(width: 10),
                 IconButton(
@@ -87,9 +75,12 @@ class ButtomBar extends StatelessWidget {
                         "https://www.facebook.com/profile.php?id=100007226413243";
                     js.context.callMethod("open", [userUrl]);
                   },
-                  iconSize: 40,
+                  iconSize: 30,
                   padding: const EdgeInsets.all(0),
-                  icon: Image.asset("assets/icons/Facebook.png"),
+                  icon: const Icon(
+                    FontAwesomeIcons.facebook,
+                    size: 30,
+                  ),
                 ),
               ],
             )
@@ -97,5 +88,35 @@ class ButtomBar extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void copyText(String s, BuildContext context) {
+    SnackBar snackBar = SnackBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      padding: EdgeInsets.fromLTRB(
+        Design.getMatirialWidth(context) * 0.4,
+        10,
+        Design.getMatirialWidth(context) * 0.4,
+        10,
+      ),
+      content: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(70, 0, 0, 0),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        height: 40,
+        child: Center(
+          child: Text(
+            'Copied to clipboard',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ),
+      ),
+      duration: const Duration(seconds: 1),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+    Clipboard.setData(ClipboardData(text: s));
   }
 }
